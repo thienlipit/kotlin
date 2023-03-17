dd3ff5dbc25bffbbef366c2859b31aa3723053ba
thienlipit <tranthienrs24@gmail.com>
2023-03-17 10:55:12 +0700

    kiem tra nghiem

---
+++ src/Zn/PTBac2.kt
@@ -0,0 +1,11 @@
+package Zn
+
+import kotlin.math.sqrt
+
+fun  main(){
+    for (k in 0..35){
+        // y^2 = 9 Kiem tra tat ca cac nghiem co the xay ra trong Z35.
+        println(sqrt(9.0 + k*35))
+    }
+
+}
